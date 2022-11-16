3dee01b3994efe5438abd84415e30023409d7173
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-11-16 11:58:41 +0100

    i tried

--- motorTraccion.asm
+++ motorTraccion.asm
@@ -18,24 +18,20 @@ unsigned short frec
 unsigned short pasos
 unsigned char movAnt=0
 
-
-
 obtener y ejecutar las consignas de movimiento del vehículo, generando los cambios de fase correspondientes. Cada consigna se 
 obtendrá con una llamada a la rutina siguiente_movimiento()
-
-
-
 
 
 void main {
 
 inicializaciones()
+
 do{
 
 tareas_independientes()
 if(movAnt=0)   @modificar desde la rsi
     movAnt=1
-    siguiente_movimiento(vel, avn)
+    siguiente_movimiento(&vel, &avn)
     signed short avnInicial
         do{
             if(pasos >= 243){
@@ -96,7 +92,7 @@ push{lr}
 ldr r0, =REG_IF
 ldrb r1,[r0]
 tst r1, #1
-bne activar_timer0
+bne activar_timer00
 
 
 
